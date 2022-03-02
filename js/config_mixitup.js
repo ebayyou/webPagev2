c7baad24f27a8ee12e313da9952f4249dfc90792
ebayyou <ebayyouanggoro.e@gmail.com>
2022-03-02 12:54:43 -0800

    Initilize & added file .gitignore

---
+++ js/config_mixitup.js
@@ -0,0 +1,11 @@
+// Todo /* ========== Mixitup : Filter portfolio ========== */
+
+// ? Example: Passing a configuration object
+var mixerFilters = mixitup('.work_container', {
+  selectors: {
+    target: '.card',
+  },
+  animation: {
+    duration: 300,
+  },
+});
